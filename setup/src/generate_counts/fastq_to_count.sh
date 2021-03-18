5e368507c81c646b72fe90e566e2029e8d60dc09
otsukaresamaja <christophersugai@gmail.com>
2021-03-17 22:24:51 -0500

    bug

--- setup/src/generate_counts/fastq_to_count.sh
+++ setup/src/generate_counts/fastq_to_count.sh
@@ -7,7 +7,7 @@ mkdir "$star_out_directory"
 
 for file_name in $(ls *.fastq.gz | sed -r 's/_R[12]_001[.]fastq[.]gz//' | uniq); do
 
-    # Make directories for samples and fastqc outputs
+    # Make subdirectories for sample outputs
     counts_file_name_directory="$star_out_directory""$file_name"
     mkdir "$counts_file_name_directory"
     cd "$counts_file_name_directory"
@@ -26,11 +26,13 @@ for file_name in $(ls *.fastq.gz | sed -r 's/_R[12]_001[.]fastq[.]gz//' | uniq);
     zcat "$R1gz" | fastqc stdin -t "$threads" -o "$r1_fastqc_directory"
     zcat "$R2gz" | fastqc stdin -t "$threads" -o "$r2_fastqc_directory"
 
-    # # Trimmomatic to process read 1
-    java -jar "$script_directory""/Trimmomatic-0.39/trimmomatic-0.39.jar" SE -phred33 -threads "$threads" "$R1gz" "$trimmomatic_directory""minlencrop_""$R1" MINLEN:"$R1_length" CROP:"$R1_length" # Remove R1 shorter than $R1_length, crop all R1 reads to $R1_length
+    # Trimmomatic to process read 1
+    java -jar "$script_directory""/Trimmomatic-0.39/trimmomatic-0.39.jar" SE -phred33 -threads "$threads" "$R1gz" "$trimmomatic_directory""minlencrop_""$R1" MINLEN:"$R1_length" CROP:"$R1_length" # Remove R1 shorter than $R1_length, crop all R1 reads to $R1_length 
     pigz -f "$trimmomatic_directory""minlencrop_""$R1"
-    java -jar "$script_directory""/Trimmomatic-0.39/trimmomatic-0.39.jar" PE -phred33 -threads "$threads" "$trimmomatic_directory""minlencrop_""$R1"".gz" "$R2gz" "$trimmomatic_directory""paired_""$R1"  "$trimmomatic_directory""singles_""$R1" "$trimmomatic_directory""paired_""$R2"  "$trimmomatic_directory""singles_""$R2" LEADING:"3" # Remove singletons
+    java -jar "$script_directory""/Trimmomatic-0.39/trimmomatic-0.39.jar" PE -phred33 -threads "$threads" "$trimmomatic_directory""minlencrop_""$R1"".gz" "$R2gz" "$trimmomatic_directory""paired_""$R1"  "$trimmomatic_directory""singles_""$R1" "$trimmomatic_directory""paired_""$R2"  "$trimmomatic_directory""singles_""$R2" LEADING:"0" # Remove singletons
     pigz -f "$trimmomatic_directory"*
+
+    # Align/count with STAR
     STAR --runThreadN "$threads" --soloType CB_UMI_Simple \
     --soloUMIlen "$umi_length" \
     --soloBarcodeReadLength "$R1_length" \
